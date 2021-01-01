0616772d1b5a6d13922af6c78db96c960837c90a
Bharadwaj <bharath.krishnan02@gmail.com>
2021-01-01 19:32:12 +0100

    Stack Class update

--- Data-Structures/src/Custom-Stack/Stack.h
+++ Data-Structures/src/Custom-Stack/Stack.h
@@ -16,12 +16,50 @@ class Stack
 {
 private:
 	T* m_stack;
+	int m_sp;
 
 public:
 	// Constructor
 	Stack()
 	{
 		m_stack = new T[N];
+		m_sp = 0;
+	}
+
+	// Add data to the stack
+	bool insert(T data)
+	{
+		bool ret = false;
+
+		// Check if stack is full
+		if(m_sp < N)
+		{
+			m_stack[m_sp++] = data;
+
+			ret = true;
+		}
+
+		return ret;
+	}
+
+	// Get data from the stack
+	T pop()
+	{
+		T ret = -1;	// Default return value
+
+		// Check if stack is empty
+		if(m_sp)
+		{
+			ret = m_stack[--m_sp];
+		}
+
+		return ret;
+	}
+
+	// Returns stack size
+	size_t size()
+	{
+		return N;
 	}
 
 	// Destructor
